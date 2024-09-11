d516538d15ff49c1e13f6ad339ab3f60e8a8ae32
Abdsi8a <abdsii8a@gmail.com>
2024-09-11 10:39:33 +0300

    e

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -178,7 +178,7 @@ class ProductController extends Controller
                 'the_product' => $sukerProducts,
             ], 200);
         }
-  
+        //f
         return response()->json([
             'the_product' => $sukerProducts,
         ], 200);
