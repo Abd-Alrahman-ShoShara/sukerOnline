cb1d010e178d2b1162d083394bec3382de759d78
Abdsi8a <abdsii8a@gmail.com>
2025-08-17 10:25:38 +0300

    d

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -15,8 +15,8 @@ class SettingsTableSeeder extends Seeder
         Setting::updateOrCreate(['key' => 'Num1'], ['value' => '123456789']);
         Setting::updateOrCreate(['key' => 'Num2'], ['value' => '987654321']);
         Setting::updateOrCreate(['key' => 'Num3'], ['value' => '555555555']);
-        Setting::updateOrCreate(['key' => 'storePrice'], ['value' => 10]);
-        Setting::updateOrCreate(['key' => 'urgentPrice'], ['value' => 20]);
+        Setting::updateOrCreate(['key' => 'storePrice'], ['value' => "10"]);
+        Setting::updateOrCreate(['key' => 'urgentPrice'], ['value' => "20"]);
         Setting::updateOrCreate(['key' => 'isActive'], ['value' => true]);
     }
 }
